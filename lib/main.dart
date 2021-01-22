c4090f40b45fe6431159a9e802b91d4ea1d66d64
20106 <saraalgazar709@gmail.com>
2021-01-22 02:24:47 +0200

    Initial Project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:team_gp/Screens/Login/login.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Login(),
+    );
+  }
+}
+
+
+
+
+
